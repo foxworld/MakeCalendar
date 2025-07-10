ed479f86c2efcb52dd15a0e9a489966f7c5175ee
foxworld <foxworld@gmail.com>
2025-07-10 22:23:36 +0900

    Make Calendar Init

--- docs/schema.sql
+++ docs/schema.sql
@@ -2,6 +2,6 @@ create table pgcal02 (
     country_code char(3) not null,
     trd_date  char(8) not null,
     day_of_week char(1) not null,
-    legal_holiday char(8) default 'N' not null
+    legal_holiday char(1) default 'N' not null
 );
 create unique index pgcal02_pk on pgcal02(country_code,trd_date);
